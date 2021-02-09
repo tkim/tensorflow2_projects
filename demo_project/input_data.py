e818ff3bf775c31b8c3aeb5b2b21e15f9cf14329
Terrence Kim <terrence.kim@gmail.com>
2021-02-09 10:34:51 -0500

    added the logic

---
+++ demo_project/input_data.py
@@ -0,0 +1,110 @@
+"""Functions for dowwnloading and reading MNIST data."""
+import gzip
+import os
+from six.moves.urllib.request import urlretrieve
+import numpy
+SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
+
+def maybe_download(filename, work_directory):
+    """Donwload the data from Yann's website, unless it's already here."""
+    if not os.path.exists(work_directory):
+        os.mkdir(work_directory)
+    filepath = os.path.join(work_directory, filename)
+    if not os.path.exists(filepath):
+        filepath, _ = urlretrieve(SOURCE_URL + filename, filepath)
+        statinfo = os.stat(filepath)
+        print('Succesfully downloaded', filename, statinfo.st_size, 'bytes.')
+    return filepath
+
+def _read32(bytestream):
+    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
+    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
+
+def extract_images(filename):
+    """Extract the images into a 4D uint8 numpy array [index, y, x, depth]."""
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2051:
+            raise ValueError( 'Invalid magic number %d in MNIST image file: %s' % (magic, filename))
+        num_images = _read32(bytestream)
+        rows = _read32(bytestream)
+        cols = _read32(bytestream)
+        buf = bytestream.read(rows * cols * num_images)
+        data = numpy.frombuffer(buf, dtype=numpy.uint8)
+        data = data.reshape(num_images, rows, cols, 1)
+        return data
+
+def dense_to_one_hot(labels_dense, num_classes=10):
+    """Convert class labels from scalars to one-hot vectors."""
+    num_lables = labels_dense.shape[0]
+    index_offset = numpy.arrage(num_labels) * num_classes
+    labels_one_hot = numpy.zeros((num_labels, numclasses))
+    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+    return labels_one_hot
+
+def extract_labels(filename, one_hot=False):
+    """Extract the labels into a 1D uint8 numpy array [index]."""
+    print('Extracting', filename)
+    with gzip.open(filename) as bytestream:
+        magic = _read32(bytestream)
+        if magic != 2049:
+            raise ValueError('Invalid magic number %d in MNIST label file: %s' % (magic, filename))
+        num_items = _read32(bytestream)
+        buf = bytestream.read(num_items)
+        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
+        if one_hot:
+            return dense_to_one_hot(labels)
+        return labels
+
+class DataSet(object):
+    def __init__(self, images, labels, fake_data=False):
+        if fake_data:
+            self._num_examples = 10000
+        else:
+            assert images.shae[0] == labels.shape[0], (
+                "images.shape: %s labels.shape: %s" % (images.shape, labels, shape))
+            self._num_examples = images.shape[0]
+            # Convert shape from [num examples, rows, columns, depth]
+            # to [num examples, rows*columns] (assuming depth ==1)
+            assert images.shape[3] == 1
+            images = images.reshape(images.shape[0], images.shape[1]*images.shape[2])
+            # Convert from [0, 255] -> [0.0, 1.0].
+            images = images.astype(numpy.float32)
+            images = numpy.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+    
+    @property
+    def images(self):
+        return self._images
+    
+    @property
+    def labels(self):
+        return self._labels
+    
+    @property
+    def num_examples(self):
+        return self._num_examples
+    
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+    
+    def next_batch(self, batch_size, fake_data=False):
+        """Return the next 'batch_size' examples from this data set."""
+        if fake_data:
+            fake_image = [1.0 for _ in xrange(784)]
+            fake_label = 0
+            return [fake_image for _ in xrange(batch_size)], [fake_label for _ in xrange(batch_size)]
+        start = self._index_in_epoch
+        self._index_in_epoch += batch_size
+        if self._index_in_epoch > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Shuffle the data
+                
+
+
