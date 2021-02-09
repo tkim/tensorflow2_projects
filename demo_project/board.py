58076ca7af4c7b6eeb1cb194a2a3f770b518b078
Terrence Kim <terrence.kim@gmail.com>
2021-02-09 15:19:36 -0500

    bug fix

--- demo_project/board.py
+++ demo_project/board.py
@@ -1,4 +1,4 @@
-# Import MINST data
+# Import MNIST data
 import input_data
 mnist = input_data.read_data_sets("data/", one_hot=True)
 
@@ -16,14 +16,14 @@ y = tf.placeholder("float", [None, 10]) # 0-9 digits recognition => 10 classes
 
 # Create a model
 
-# Set a model weights
+# Set model weights
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
 with tf.name_scope("Wx_b") as scope:
     # Construct a linear model
-    model = tf.nn.softmax(tf.matmul(x, W) + b) #Softmax
-
+    model = tf.nn.softmax(tf.matmul(x, W) + b) # Softmax
+    
 # Add summary ops to collect data
 w_h = tf.summary.histogram("weights", W)
 b_h = tf.summary.histogram("biases", b)
@@ -41,7 +41,7 @@ with tf.name_scope("train") as scope:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
 
 # Initializing the variables
-init = tf.initalize_all_variables()
+init = tf.initialize_all_variables()
 
 # Merge all summaries into a single operator
 merged_summary_op = tf.summary.merge_all()
@@ -50,8 +50,10 @@ merged_summary_op = tf.summary.merge_all()
 with tf.Session() as sess:
     sess.run(init)
 
-    # Location on my computer
-    summary_writer = tf.summary.FileWriter('data\logs', graph_def=sess.graph_def)
+    
+    
+    # Change this to a location on your computer
+    summary_writer = tf.summary.FileWriter('data/logs', graph_def=sess.graph_def)
 
     # Training cycle
     for iteration in range(training_iteration):
@@ -63,22 +65,18 @@ with tf.Session() as sess:
             # Fit training using batch data
             sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys})
             # Compute the average loss
-            avg_cost += sess.run(cost_function, feed_dict={x: batch xs, y:batch ys})/total_batch
+            avg_cost += sess.run(cost_function, feed_dict={x: batch_xs, y: batch_ys})/total_batch
             # Write logs for each iteration
-            summary_str = sess.run(merged_summary_op, feed_dict={x: batch_xs, y:batch_ys})
+            summary_str = sess.run(merged_summary_op, feed_dict={x: batch_xs, y: batch_ys})
             summary_writer.add_summary(summary_str, iteration*total_batch + i)
         # Display logs per iteration step
         if iteration % display_step == 0:
-            print("Iteration:", %04d % (iteration +1), "cost=", "{:.9f}".format(avg_cost))
-    
+            print("Iteration:", '%04d' % (iteration + 1), "cost=", "{:.9f}".format(avg_cost))
+
     print("Tuning completed!")
 
     # Test the model
     predictions = tf.equal(tf.argmax(model, 1), tf.argmax(y, 1))
     # Calculate accuracy
     accuracy = tf.reduce_mean(tf.cast(predictions, "float"))
-    print("Accuracy:", accuracy.eval({x: mnist.test.images, y:mnist.test.labels}))
-
-
-
-
+    print("Accuracy:", accuracy.eval({x: mnist.test.images, y: mnist.test.labels}))
